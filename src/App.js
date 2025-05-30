dae0db4d18b6791b0a670add1a4966242a00d08f
Deepak Kumar <114341053+deepakgu3011@users.noreply.github.com>
2025-05-30 12:27:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from './pages/about';
 import Contact from './pages/contact';
 import Privacy from './pages/privacy_policy';
 import Service from './pages/service'; 
-import AdminService from './pages/admin/addservice'; 
 import NotFound from './pages/error/404';
 import Server from './pages/error/500';
 import Footer from './main/footer';
@@ -22,7 +21,6 @@ function App() {
         <Route path="/privacy-policy" element={<Privacy />} />
         <Route path="/services" element={<Service />} /> 
 
-        <Route path="/admin/add/service" element={<AdminService />} />  {/* Adding Service page route */}
         {/* Adding Service page route */}
         <Route path="*" element={<NotFound />} /> {/* Redirect to Home for any unmatched routes */}
         <Route path="/404" element={<NotFound />} /> {/* Custom 404 page can be added later */}
